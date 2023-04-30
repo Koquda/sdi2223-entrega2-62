ab386c528bfa8ef29a02efc338c36754b6528504
UO284622 <UO284622@uniovi.es>
2023-04-30 20:39:50 +0200

    S4 y S5 funcionando

--- wallapopNode/repositories/conversationsRepository.js
+++ wallapopNode/repositories/conversationsRepository.js
@@ -17,6 +17,38 @@ module.exports = {
             throw (error);
         }
     },
+    findAllConversationFilter: async function (filter, options) {
+        try {
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("wallapopDB");
+            const collectionName = 'conversations';
+            const conversationsCollection = database.collection(collectionName);
+            const conversation = await conversationsCollection.find(filter,options).toArray()
+
+            return conversation;
+        } catch (error) {
+            throw (error);
+        }
+    },
+    findAllConversationGroupOfferId: async function (filter, options) {
+        try {
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("wallapopDB");
+            const collectionName = 'conversations';
+            const conversationsCollection = database.collection(collectionName);
+            const conversation = await conversationsCollection.find(filter,options).toArray()
+
+            const pipeline = [
+                { $match: filter }, // opcional, si desea filtrar documentos
+                { $group: { _id: "$offerID", messages: { $push: "$$ROOT" } } } // agrupa por offerID
+            ];
+
+            const conversations = await conversationsCollection.aggregate(pipeline, options).toArray();
+            return conversations;
+        } catch (error) {
+            throw (error);
+        }
+    },
     insertConversation: async function (conversation) {
         try {
             const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
